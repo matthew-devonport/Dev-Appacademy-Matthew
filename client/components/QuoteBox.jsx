680fe829d84f1fa17642cb7af2f9509d7e08561d
noemie-wandering-anemone <43393997+noemie-wandering-anemone@users.noreply.github.com>
2019-09-25 20:57:52 +1200

    kinda working

---
+++ client/components/QuoteBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class QuoteBox extends React.Component {
+
+  render() {
+    return (
+      <p>Quote here</p>
+    )
+  }
+}
+
+export default QuoteBox
--- client/components/Room.jsx
+++ client/components/Room.jsx
@@ -5,9 +5,42 @@ import { changePage } from '../actions'
 
 class Room extends React.Component {
 
+    state = {
+        quoteIsShowing: false,
+        interval: null
+    }
+
+    componentDidMount() {
+        console.log('mounted boiii')
+        this.setQuoteInterval()
+    }
+
+    componentWillUnmount() {
+        console.log('unmounted')
+        clearInterval(this.state.interval)
+    }
+
+    setQuoteInterval = () => {
+        let interval = setInterval(this.toggleQuote, 1000)
+        this.setState({
+            interval: interval
+        })
+    }
+
+    toggleQuote = () => {
+        this.setState({
+            quoteIsShowing: !this.state.quoteIsShowing
+        })
+        console.log(this.state)
+    }
+
     handleClick = (pageNum) => {
         const { dispatch } = this.props
         dispatch(changePage(pageNum))
+    }
+
+    createTextBox = () => {
+        console.log(this.state)
     }
 }
 
