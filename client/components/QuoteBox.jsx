6567e0ef777490e14e24fe6c7feead68a3e49703
Alex-Abernethy <alexander.abernethy@gmail.com>
2019-09-26 17:43:31 +1200

    box position can now be easily changed from other components

--- client/components/QuoteBox.jsx
+++ client/components/QuoteBox.jsx
@@ -3,9 +3,17 @@ import { connect } from 'react-redux';
 
 
 class QuoteBox extends React.Component {
+  
+  componentDidMount() {
+    let { top, left } = this.props.quoteBox;
+    let box = document.getElementById('quote-box');
+    box.style.top = top;
+    box.style.left = left;
+  }
 
   render() {
     let {quoteBox} = this.props
+    
     return (
       <div id="quote-box">{quoteBox.quote}</div>
     )
