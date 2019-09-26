96c2fc32bcfa5fd7d3bbbb2a50ed3088c18827e3
noemie-wandering-anemone <43393997+noemie-wandering-anemone@users.noreply.github.com>
2019-09-26 16:27:19 +1200

    Position clicables elements relative to the background picture

--- client/components/MainRoom.jsx
+++ client/components/MainRoom.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Jv from './Jv'
 import Han from './Han'
 import Ollie from './Ollie'
-import Sarah from './Sarah'
+import Sarrah from './Sarrah'
 import Kelly from './Kelly'
 import { connect } from 'react-redux'
 import Room from './Room'
@@ -14,20 +14,19 @@ class MainRoom extends Room {
 
   render() {
     return (
-      <div>
-        {this.props.page == 0 && <div>
-          <p>You are on the homepage</p>
-          <button onClick={() => this.handleClick(1)}>JV</button>
-          <button onClick={() => this.handleClick(2)}>Han</button>
-          <button onClick={() => this.handleClick(3)}>Ollie</button>
-          <button onClick={() => this.handleClick(4)}>Sarah</button>
-          <button onClick={() => this.handleClick(5)}>Kelly</button>
+      <div id="content">
+        {this.props.page == 0 && <div id='mainroom'>
+          <div className="teachers" id="ollie" onClick={() => this.handleClick(3)}>Ollie</div>
+          <div className="teachers" id="han" onClick={() => this.handleClick(2)}>Han</div>
+          <div className="teachers" id="jv" onClick={() => this.handleClick(1)}>JV</div>
+          <div className="teachers" id="sarrah" onClick={() => this.handleClick(4)}>Sarrah</div>
+          <div className="teachers" id="kelly" onClick={() => this.handleClick(5)}>Kelly</div>
         </div>}
 
         {this.props.page == 1 && <Jv />}
         {this.props.page == 2 && <Han />}
         {this.props.page == 3 && <Ollie />}
-        {this.props.page == 4 && <Sarah />}
+        {this.props.page == 4 && <Sarrah />}
         {this.props.page == 5 && <Kelly />}
         <QuoteBox />
       </div>
--- client/components/Sarrah.jsx
+++ client/components/Sarrah.jsx
@@ -3,16 +3,16 @@ import { connect } from 'react-redux'
 import Room from './Room'
 
 
-class Sarah extends Room {
+class Sarrah extends Room {
 
   render() {
     return (
       <div>
-        <p>Hello, I am Sarah</p>
+        <p>Hello, I am Sarrah</p>
         <button onClick={() => this.handleClick(0)}>Home</button>
       </div>
     )
   }
 }
 
-export default connect()(Sarah)
+export default connect()(Sarrah)
