48c4490357ab7258c43c70916dbb92d69e6c9283
Alex-Abernethy <alexander.abernethy@gmail.com>
2019-09-26 16:06:25 +1200

    done action tests

--- tests/client/actions.test.js
+++ tests/client/actions.test.js
@@ -6,3 +6,11 @@ test('changePage', () => {
     expect(action.type).toBe('CHANGE_PAGE')
     expect(action.page).toBe(3)
 })
+
+test('setQuoteBox', () => {
+    let action = actions.setQuoteBox('hi', '20vh', '30vh');
+    expect(action.type).toBe('SET_QUOTEBOX')
+    expect(action.quote).toBe('hi')
+    expect(action.top).toBe('20vh');
+    expect(action.left).toBe('30vh');
+})
