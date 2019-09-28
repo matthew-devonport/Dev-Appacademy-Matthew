b5cdd8d5da0a906705cd05587fde4fb5040f6116
Sylvia Gilbert-Potts <sylvia.gilbert.potts@hotmail.com>
2019-09-28 19:24:51 +1200

    fix merge issues

--- client/components/Ollie.jsx
+++ client/components/Ollie.jsx
@@ -15,40 +15,20 @@ class Ollie extends Room {
     })
   }
   name = 'Ollie'
-<<<<<<< HEAD
-<<<<<<< HEAD
   top = '10vh'
   left = '30vw'
-=======
-  top = '20vh'
-  left = '20vw'
 
   popupContent = ``
   
->>>>>>> 5c0ec91cafca1b406f99a8838126ac9f7ddf9e0d
-=======
-  top = '10vh'
-  left = '30vw'
->>>>>>> 10da396850dfb6d00b9d5c1b25e29d8bc3669c8d
   render() {
     return (
       <div className='room' id="ollieRoom">
         <p>Hello, I am Ollie</p>
         <button onClick={() => this.handleClick(0)}>Home</button>
         {this.state.quoteIsShowing && <QuoteBox />}
-<<<<<<< HEAD
-<<<<<<< HEAD
         {/* <h1>This is where our questions will go!</h1>
-        {this.state.questions && this.state.questions.map((question, i) => <p key={i}>{question}</p>)} */}
-=======
-        <h1>This is where our questions will go!</h1>
         {this.state.questions &&
-          this.state.questions.map((question, i) => <p key={i}>{question}</p>)}
->>>>>>> 5c0ec91cafca1b406f99a8838126ac9f7ddf9e0d
-=======
-        {/* <h1>This is where our questions will go!</h1>
-        {this.state.questions && this.state.questions.map((question, i) => <p key={i}>{question}</p>)} */}
->>>>>>> 10da396850dfb6d00b9d5c1b25e29d8bc3669c8d
+          this.state.questions.map((question, i) => <p key={i}>{question}</p>)} */}
       </div>
     )
   }
