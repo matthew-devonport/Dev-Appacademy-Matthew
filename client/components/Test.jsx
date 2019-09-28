264aedbd9252af51c08d5eea3a25a2da23a90b8e
Caleb Thompson <52016020+ccthompson-it@users.noreply.github.com>
2019-09-28 16:06:07 +1200

    changed popup to be independant

---
+++ client/components/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div>
+      <h1>You clicked the head!</h1>
+      <p>Congratulations!</p>
+    </div>
+  )
+}
+
+export default Test
