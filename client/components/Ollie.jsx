95c11497cdde61e6fc662e0ade9adfa1a2a584ca
Caleb Thompson <52016020+ccthompson-it@users.noreply.github.com>
2019-09-29 16:08:49 +1300

    add audio element to component

--- client/components/Ollie.jsx
+++ client/components/Ollie.jsx
@@ -9,40 +9,44 @@ import { getQuestions } from '../apiClient'
 class Ollie extends Room {
   constructor(props) {
     super(props)
+    this.state = {
+      play: false,
+      questions: ''
+    }
     getQuestions().then(result => {
       this.setState({
-        questions: result.questions
-      })
+        questions:
+      result.questions.map((question, i) => <p id ='QOTD' key={i}>{question}</p>)  
+    })
+    this.audio = new Audio("../../sounds/groupClap.mp3")
+    this.play = this.play.bind(this)
     })
   }
   name = 'Ollie'
   top = '10vh'
   left = '30vw'
 
-  // questions = this.state.questions &&
-  //         this.state.questions.map((question, i) => <p key={i}>{question}</p>)
-
-  popupContent = (
-    <div>
-      <h1>Question of the day!</h1>
-      <h1>This is where our questions will go!</h1>
-      {console.log(this.state.questions)}
-    </div>
-  )
+  play(){
+    this.setState({
+      play: true
+    })
+    this.audio.play()
+  }
   
   render() {
+    console.log(this.audio)
     return (
       <div className='room' id="ollieRoom">
-        <p>Hello, I am Ollie</p>
         <button onClick={() => this.handleClick(0)}>Home</button>
         {this.state.quoteIsShowing && <QuoteBox />}
-      <PopUpBox
-      content={this.popupContent}
+      {this.state.questions && <PopUpBox
+      content={<div><h1 id='QOTD-Title'>Question of the day:</h1>{this.state.questions[Math.floor(Math.random() * 19)]}</div>}
       top='5vh'
       left='5vw'
-      height='5vh'
-      width='20vw'
-      />
+      height='7vh'
+      width='25vw'
+      />}
+      <button onClick={this.play}>Clap</button>
       </div>
     )
   }
