a8fb7e994c1765724d182ab34d8dbed7bece4544
noemie-wandering-anemone <43393997+noemie-wandering-anemone@users.noreply.github.com>
2019-09-30 15:56:54 +1300

    Remove constructor

--- client/components/PopUpBox.jsx
+++ client/components/PopUpBox.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 
 class PopUpBox extends React.Component {
-  constructor(props) {
-    super(props)
-    let { content } = this.props
-    this.state = {
-      content
-    }
-  }
-
 
   render() {
-    let { content } = this.state
+    let { content } = this.props
     return (
       <React.Fragment>
         <div id='popup-overlay'>
