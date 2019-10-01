21afedd6e41a224fdc668fdeb315952240945128
noemie-wandering-anemone <43393997+noemie-wandering-anemone@users.noreply.github.com>
2019-10-01 14:25:45 +1300

    Test all mindfulness technics are displayed

--- tests/client/components/Han.test.js
+++ tests/client/components/Han.test.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import Enzyme, {shallow, render, mount} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
 
-Enzyme.configure({adapter: new Adapter()})
+jest.mock('../../../client/components/Room')
+jest.mock('react-redux', () => {
+    return {
+        connect: () => {
+            return (component) => component 
+        }
+    }
+})
+import '../config/enzyme'
+
+import Han from '../../../client/components/Han'
+import { JestEnvironment } from '@jest/environment';
+// Han.prototype.componentDidMount = () => {}
 
 test('test runner is working', () => {
   expect(true).toBeTruthy()
 })
 
-describe('<Han /> component', () => {
-  test('test', (done) => {
-    done()
+describe('<Han> component', () => {
+  test('<Han> has 8 technics', () => {
+        const wrapper = shallow(<Han />)
+        const actual = wrapper.find('.technic')
+        expect(actual.length).toBe(8)
+
   })
   test('test', (done) => {
     done()
