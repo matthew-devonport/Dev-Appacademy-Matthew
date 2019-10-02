7bb8197d3e7be38bb021575251769811db3c1a75
noemie-wandering-anemone <43393997+noemie-wandering-anemone@users.noreply.github.com>
2019-10-02 18:16:49 +1300

    Add mindfulness content

--- client/components/Mindfulness.jsx
+++ client/components/Mindfulness.jsx
@@ -3,25 +3,34 @@ import React from 'react'
 const Mindfulness = () => {
     return (
         <React.Fragment>
-           <h1 className='popupTitle'>Mindfulness</h1>
-           <p>Mindfulness is a form of meditation. 
-               It's paying attention, on purpose, in the present moment, non judgmentally, simply noticing what you are experiencing, without criticisms or judgments or evaluations. 
-               Mindfulness helps us develop our attention and meta-attention. 
-               We can pay attention to breathing sensations in the belly and be aware when the attention is somewhere else. 
+            <h1 className='popupTitle'>Mindfulness</h1>
+            <div id='technics-container'>
+                <div>
+                    <p><span className='technic-title'>Mindfulness</span> is a form of meditation.
+                        It's paying attention, on purpose, in the present moment, non judgmentally, simply noticing what you are experiencing, without criticisms or judgments or evaluations.
+                        Mindfulness helps us develop our attention and meta-attention.
+                        We can pay attention to breathing sensations in the belly and be aware when the attention is somewhere else.
                Its fairly remarkable quality we have as humans, not just to be aware of something, but to witness ourselves being aware of it.</p>
-           <h2>Sounds</h2><p>Mindfulness is a form of meditation</p>
-           <h2>Body Scan</h2>
-           <p>Mindfulness is a form of meditation</p>
-           <h2>Deep Breathing</h2>
-           <p>Mindfulness is a form of meditation</p>
-           <h2>Here & Now</h2>
-           <p>Mindfulness is a form of meditation</p>
-           <h2>R.A.I.N</h2>
-           <p>Mindfulness is a form of meditation</p>
-           <h2>5 Senses</h2>
-           <p>Mindfulness is a form of meditation</p>
-           <h2>Metta</h2>
-           <p>Mindfulness is a form of meditation dvnqou;bgwr ;qwgw</p>
+                    <p><span className='technic-title'>Sounds</span> With this technique, you'll be focusing on the sounds around you, noticing them without feeling the need to label them.</p>
+                    <p><span className='technic-title'>Body Scan</span> Starting from your feet, going up to the head, bring your awareness to each part of your body.</p>
+                    <p><span className='technic-title'>Deep Breathing</span> Find the place in your body when you can fell your breath the most. As you breath, and without changing your spontaneous breath, notice the movement of the air. The mind can focus on counting the breaths, up to 10, and start over again.</p>
+                    <p><span className='technic-title'>Here & Now</span> This technique combines the first 3 ones to anchor you in the present moment.</p>
+                </div>
+                <div>
+                    <p><span className='technic-title'>R.A.I.N</span> a pratice to cultivate self-compassion. 4 steps to follow when feeling overwhelmed:<br />
+                        <ul>
+                            <li>Recognize what's going on,</li>
+                            <li>Allow the experience to be there, just as it is</li>
+                            <li>Investigate with kindness</li>
+                            <li>Natural awareness, which comes from not identifying with the experience</li>
+                        </ul></p>
+                    <p><span className='technic-title'>5 Senses</span> A Mindfulness technique done with opened eyes. Count 5 things you can see, 4 things you can fell, 3 things you can hear, 2 things you can smell, 1 thing you can taste.</p>
+                    <p><span className='technic-title'>Metta</span> A meditation of Love of Kindness. Start by wishing yourself "May I be well. May I be happy. May I be healthy. May I be free from suffering".<br />
+                    Then bring someone you care about to your mind and wish them: "May you be well. May you be happy. May you be healthy. May you be free from suffering".<br />
+                    And someone who you are not getting along with and wish them: "May you be well. May you be happy. May you be healthy. May you be free from suffering".<br />
+           Finally, expend your attention to all linving beings and wish them: ‘‘May all be well. May all be happy. May all be healthy. May all be free from suffering’.</p>
+                </div>
+            </div>
         </React.Fragment>
     )
 }
