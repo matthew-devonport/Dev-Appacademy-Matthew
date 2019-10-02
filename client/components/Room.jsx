444bd063bae401f67034b82746f7db0719ea1285
Nicholas-Matthews <nichmattnz@gmail.com>
2019-10-02 14:55:45 +1300

    fixed quote box on a couple of components

--- client/components/Room.jsx
+++ client/components/Room.jsx
@@ -33,7 +33,7 @@ class Room extends React.Component {
   }
 
   setQuoteInterval = () => {
-    let interval = setInterval(this.toggleQuote, 15000)
+    let interval = setInterval(this.toggleQuote, 3000)
     this.setState({
       interval: interval
     })
@@ -51,7 +51,7 @@ class Room extends React.Component {
           quoteIsShowing: !this.state.quoteIsShowing
         })
       }
-    }, 3000)
+    }, 1000)
   }
 
   setQuote = () => {
